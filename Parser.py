4d4dd9e8e0ac79530482c68d4bfa23a5b12bd7e5
Christian Kjær Laustsen <christianlaustsen@gmail.com>
2013-02-10 05:43:09 +0100

    Restructure files. Pass all tests so far (aka naive implementation).

--- Parser.py
+++ Parser.py
@@ -1,7 +1,32 @@
-def parse(s, output='list'):
-    t = tokenize(s)
-    print t
-    print check_for_IRC_code(t)
+"""
+Parse IRC output.
+
+The main function of this module is the `parse` function. It takes in some
+IRC output, and generates a tuple/dict/list looking like the following:
+{
+    'server': 'irc.codetalk.io',
+    'channel': '#lobby',
+    'user': None,
+    'code': 332,
+    'action': None,
+    'msg': 'Let us all have great fun! :D'
+}
+    
+This will indicate that a topic has been found for a channel. An example of 
+a user action would be:
+
+{
+    'server': None,
+    'channel': '#lobby',
+    'user': ('Tehnix', 'Tehnix', 'ghost-EC31B3C1.rdns.scalabledns.com'),
+    'code': None,
+    'action': 'NICK',
+    'msg': 'BlaBliBlu'
+}
+
+Here, the user changed his nickname from 'Tehnix' to 'BlaBliBlu'.
+
+"""
 
 def tokenize(s):
     s = s.split(':')
@@ -10,32 +35,91 @@ def tokenize(s):
     else:
         return []
 
-def nickname(t):
+def getNickname(t):
     """Take in a token list, and return the nickname."""
     return t[0].split('!')[0]
 
-def stripped_nickname(s):
+def strippedNickname(s):
     """Take a nickname and strip it from excessive information."""
     if len(s) > 1 and s[0:1] in ['@', '!', '&', '~']:
         return s[1:]
     else:
         return s
 
-def is_server_message(t):
+def getHostname(t):
+    return t[0].split('!')[1].split('@')[1].split()[0]
+
+def getUser(t):
+    return t[0].split('!')[1].split('@')[0]
+
+def getChannel(t):
+    for item in t[0].split():
+        if item.startswith('#'):
+            return item
+    return None
+
+def getServer(t):
+    return t[0].split()[0]
+
+def getAction(t):
+    return t[0].split()[1]
+
+def isServerMessage(t):
     if '!' not in t[0].split()[0]:
         return True
     return False
         
-def check_for_IRC_code(t):
+def getIRCCode(t):
     c = t[0].split()[1]
     try:
         int(c)
     except ValueError:
-        return False
-    return True
+        return None
+    return c
 
-def handle_IRC_code(i, t):
-    pass
+def getMsg(t, code=None, action=None):
+    if len(t) > 1:
+        return ':'.join(t[1:])
+    return None
 
-def nicks_from_names_list(t):
-    pass
+def parse(s, output='tuple'):
+    """Parse the IRC output. By default, return a tuple. Optionally return a dict or a list."""
+    t = tokenize(s)
+    code = getIRCCode(t)
+    channel = getChannel(t)
+    if isServerMessage(t):
+        server = getServer(t)
+        user = None
+        action = None
+    else:
+        server = None
+        user = (strippedNickname(getNickname(t)), getUser(t), getHostname(t))
+        action = getAction(t)
+    msg = getMsg(t, code=code, action=action)
+    if output == 'dict' or output == 'dictionary':
+        return {
+            'server': server,
+            'channel': channel,
+            'user': user,
+            'code': code,
+            'action': action,
+            'msg': msg
+        }
+    elif output == 'list':
+        return [
+            server,
+            channel,
+            user,
+            code,
+            action,
+            msg
+        ]
+    else:
+        return (
+            server,
+            channel,
+            user,
+            code,
+            action,
+            msg
+        )
